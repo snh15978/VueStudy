6b06c92a1d6117fd012853b5634de053c86b2ad1
namhyun Sung <snh15978@gmail.com>
2020-02-06 19:52:38 +0900

    createform with axios

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <app-Header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    data: function(){
+        return{
+            str: 'header'
+        }
+    },
+    components: {
+        'app-Header': AppHeader
+    },
+    methods: {
+        renewStr: function(){
+            this.str = 'headerChange'
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      {{propsdata}}
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function(){
+            this.$emit('renew');
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
